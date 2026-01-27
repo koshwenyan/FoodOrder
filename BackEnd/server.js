85d01bec649add64096f0e1216bef9ad41410949
ko shwe <nyanshwe43@gmail.com>
2026-01-27 09:35:29 +0630

    port change

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -10,7 +10,7 @@ import userRouter from './routes/userRoute.js';
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const allowedOrigin = ['http://localhost:5173'];
 
