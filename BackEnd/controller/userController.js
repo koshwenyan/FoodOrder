61b4f9cdead1b4565b80769b4c8c8fe5297d635c
ko shwe <nyanshwe43@gmail.com>
2026-01-27 11:04:26 +0630

    pagination change

--- BackEnd/controller/userController.js
+++ BackEnd/controller/userController.js
@@ -153,7 +153,7 @@ export const getAllUsers = async (req, res) => {
 
         // Pagination
         const page = Number(req.query.page) || 1;
-        const limit = Number(req.query.limit) || 10;
+        // const limit = Number(req.query.limit) || 10;
         const skip = (page - 1) * limit;
 
         // Search
