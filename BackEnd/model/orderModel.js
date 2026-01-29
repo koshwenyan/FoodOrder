49085608a44dc33a4d22dbd94dabdf0e923941c7
ko shwe <nyanshwe43@gmail.com>
2026-01-29 14:02:57 +0630

    order

---
+++ BackEnd/model/orderModel.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+    {
+        customer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+
+        shopId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Shop",
+            required: true
+        },
+
+        items: [
+            {
+                menuId: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Menu",
+                    required: true
+                },
+                name: String,
+                price: Number,
+                quantity: Number
+            }
+        ],
+
+        totalAmount: {
+            type: Number,
+            required: true
+        },
+
+        status: {
+            type: String,
+            enum: [
+                "pending",        // customer placed
+                "accepted",       // shop accepted
+                "preparing",
+                "ready",
+                "assigned",       // admin → delivery company
+                "picked-up",      // delivery staff
+                "delivered",
+                "cancelled"
+            ],
+            default: "pending"
+        },
+
+        deliveryCompany: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "DeliveryCompany",
+            default: null
+        },
+
+        deliveryStaff: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: null
+        },
+
+        deliveryAddress: String,
+
+        isPaid: {
+            type: Boolean,
+            default: false
+        }
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("Order", orderSchema);
